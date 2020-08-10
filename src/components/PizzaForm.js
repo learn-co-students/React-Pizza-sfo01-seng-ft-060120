b3a01333dce987ae4614c91fd95c441b8130d907
iona-b <ionabrabender@gmail.com>
2020-08-09 18:23:23 -0700

    Done.

--- src/components/PizzaForm.js
+++ src/components/PizzaForm.js
@@ -1,16 +1,16 @@
 import React from "react"
 
-const PizzaForm = () => {
+const PizzaForm = (props) => {
+  
+  const {topping, size, vegetarian} = props.editedPizza
+
   return(
       <div className="form-row">
         <div className="col-5">
-            <input type="text" className="form-control" placeholder="Pizza Topping" value={
-                //Pizza Topping Should Go Here
-                null
-              }/>
+            <input type="text" className="form-control" placeholder="Pizza Topping" value={topping} name="topping" onChange={props.handleChangeInEditFormData}/>
         </div>
         <div className="col">
-          <select value={null} className="form-control">
+          <select value={size} className="form-control" name="size" onChange={props.handleChangeInEditFormData}>
             <option value="Small">Small</option>
             <option value="Medium">Medium</option>
             <option value="Large">Large</option>
@@ -18,20 +18,20 @@ const PizzaForm = () => {
         </div>
         <div className="col">
           <div className="form-check">
-            <input className="form-check-input" type="radio" value="Vegetarian" checked={null}/>
+            <input className="form-check-input" type="radio" value="Vegetarian" checked={vegetarian} name="vegetarian" onChange={props.handleChangeInEditFormData}/>
             <label className="form-check-label">
               Vegetarian
             </label>
           </div>
           <div className="form-check">
-            <input className="form-check-input" type="radio" value="Not Vegetarian" checked={null}/>
+            <input className="form-check-input" type="radio" value="Not Vegetarian" checked={vegetarian === "" ? null : !vegetarian} name="vegetarian" onChange={props.handleChangeInEditFormData}/>
             <label className="form-check-label">
               Not Vegetarian
             </label>
           </div>
         </div>
         <div className="col">
-          <button type="submit" className="btn btn-success" onClick={console.log}>Submit</button>
+          <button type="submit" className="btn btn-success" onClick={(event) => props.updatePizza(event)}>Submit</button>
         </div>
       </div>
 
